9a0cacf1fd126ec50938fde03e61dd675af517d3
Yuka Ezura <yuka.ezura@gmail.com>
2014-07-27 15:23:37 +0900

    rails g controller Comments create

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+end
