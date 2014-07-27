b9d8dfb5792207c1b085f9a2a685ca6a00b04804
Yuka Ezura <yuka.ezura@gmail.com>
2014-07-27 14:57:47 +0900

    スライドを表示できるように変更

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,2 +1,5 @@
 class Slide < ActiveRecord::Base
+  def slideshare_id
+    url.split('-').last
+  end
 end
--- spec/models/slide_spec.rb
+++ spec/models/slide_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Slide, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#slideshare_id' do
+    it 'SlideShareのIDが返されること' do
+      slide = Slide.new(url: 'http://www.slideshare.net/sinsoku/git-28491305')
+      expect(slide.slideshare_id).to eq '28491305'
+    end
+  end
 end
