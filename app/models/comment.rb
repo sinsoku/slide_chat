24c152c0dd307d2ed30b21c7a42f1f4bb732e1cc
sinsoku <sinsoku.listy@gmail.com>
2014-08-10 20:18:52 +0900

    add validates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :slide
+
+  validates :content, presence: true
+  validates :page_number, presence: true, numericality: true
 end
--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,6 +1,8 @@
 class Slide < ActiveRecord::Base
   has_many :comments
 
+  validates :url, presence: true
+
   def slideshare_id
     url.split('-').last
   end
